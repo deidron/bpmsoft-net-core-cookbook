4e87d41789d7f268e47d7b2e2127cf650f60a773
deidron <deiddron@gmail.com>
2026-09-17 00:19:48 +0300

    ci: run dotnet format in Release like build

--- .github/scripts/format.sh
+++ .github/scripts/format.sh
@@ -4,4 +4,7 @@ pkg="$1"
 sln=$(find "$pkg" -maxdepth 1 -name '*.sln'    | head -1)
 prj=$(find "$pkg" -maxdepth 1 -name '*.csproj'  | head -1)
 
+# dotnet format loads Debug by default; match build.sh (Release).
+export Configuration=Release
+
 dotnet format "${sln:-$prj}" --verify-no-changes --verbosity diagnostic --severity info
